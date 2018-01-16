190e1f2260e9e050edd9228cd8700990d08a5e9b
hsong-rh <hsong@redhat.com>
2018-01-16 09:12:53 -0500

    Changed based on comments

--- lib/fs/MiqFS/modules/XFSProbe.rb
+++ lib/fs/MiqFS/modules/XFSProbe.rb
@@ -3,7 +3,7 @@ require 'fs/xfs/superblock'
 module XFSProbe
   def self.probe(dobj)
     unless dobj.kind_of?(MiqDisk)
-      $log.debug("XFSProbe << FALSE because Disk Object class is not MiqDisk, but is '#{dobj.class}'")
+      $log&.debug("XFSProbe << FALSE because Disk Object class is not MiqDisk, but is '#{dobj.class}'")
       return false
     end
 
@@ -12,10 +12,10 @@ module XFSProbe
     XFS::Superblock.new(dobj)
 
     # If initializing the superblock does not throw any errors, then this is XFS
-    $log.debug("XFSProbe << TRUE")
+    $log&.debug("XFSProbe << TRUE")
     return true
   rescue => err
-    $log.debug("XFSProbe << FALSE because #{err.message}")
+    $log&.debug("XFSProbe << FALSE because #{err.message}")
     return false
   ensure
     dobj.seek(0, IO::SEEK_SET)
