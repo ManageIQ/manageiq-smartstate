acf66c24f28c8a4ed45c039235ed81000403fd04
hsong-rh <hsong@redhat.com>
2018-01-16 09:12:53 -0500

    Changed based on comments

--- lib/fs/MiqFS/modules/Ext4Probe.rb
+++ lib/fs/MiqFS/modules/Ext4Probe.rb
@@ -3,7 +3,7 @@ require 'fs/ext4/superblock'
 module Ext4Probe
   def self.probe(dobj)
     unless dobj.kind_of?(MiqDisk)
-      $log.debug("Ext4Probe << FALSE because Disk Object class is not MiqDisk, but is '#{dobj.class}'")
+      $log&.debug("Ext4Probe << FALSE because Disk Object class is not MiqDisk, but is '#{dobj.class}'")
       return false
     end
 
@@ -11,10 +11,10 @@ module Ext4Probe
     Ext4::Superblock.new(dobj)
 
     # If initializing the superblock does not throw any errors, then this is Ext4
-    $log.debug("Ext4Probe << TRUE")
+    $log&.debug("Ext4Probe << TRUE")
     return true
   rescue => err
-    $log.debug("Ext4Probe << FALSE because #{err.message}")
+    $log&.debug("Ext4Probe << FALSE because #{err.message}")
     return false
   ensure
     dobj.seek(0, IO::SEEK_SET)
