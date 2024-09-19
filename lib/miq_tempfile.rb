7ca3667f3a59ba92255dc5a1897e090463e11c0d
Adam Grare <adam@grare.com>
2024-09-19 08:26:38 -0400

    Fix MiqTempfile initialize kwargs error

--- lib/miq_tempfile.rb
+++ lib/miq_tempfile.rb
@@ -5,11 +5,11 @@ class MiqTempfile < DelegateClass(Tempfile)
   # TODO: share this definition with appliance console code.
   MIQ_TMP_DIR = '/var/www/miq_tmp'.freeze
 
-  def initialize(basename, *options)
+  def initialize(basename, **options)
     if File.directory?(MIQ_TMP_DIR)
-      super(Tempfile.new(basename, MIQ_TMP_DIR, *options))
+      super(Tempfile.new(basename, MIQ_TMP_DIR, **options))
     else
-      super(Tempfile.new(basename, *options))
+      super(Tempfile.new(basename, **options))
     end
   end
 end
