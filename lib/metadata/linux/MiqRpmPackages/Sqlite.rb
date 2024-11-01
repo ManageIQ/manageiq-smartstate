4672f746dc384700f031f2248e5bc5e0ab83a0a5
Adam Grare <adam@grare.com>
2024-11-01 12:58:56 -0400

    Use sqlite Packages table for rpms list

--- lib/metadata/linux/MiqRpmPackages/Sqlite.rb
+++ lib/metadata/linux/MiqRpmPackages/Sqlite.rb
@@ -1,4 +1,17 @@
 class MiqRpmPackages
   class Sqlite < MiqRpmPackages
+    def initialize(fs, dbFile)
+      require "db/MiqSqlite/MiqSqlite3"
+      @pkgDb = MiqSqlite3DB::MiqSqlite3.new(dbFile, fs)
+    end
+
+    def each
+      @pkgDb.getTable("Packages").each_row do |pkg|
+        yield pkg
+      end
+    end
+
+    def close
+    end
   end
 end
