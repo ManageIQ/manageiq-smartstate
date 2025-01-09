4ea9a51814befd0a779cd619c35d0f28e1cf0f2c
Adam Grare <adam@grare.com>
2025-01-09 15:47:44 -0500

    Extract rpmdb.sqlite from MiqFs

--- lib/metadata/linux/MiqRpmPackages/Sqlite.rb
+++ lib/metadata/linux/MiqRpmPackages/Sqlite.rb
@@ -1,4 +1,40 @@
 class MiqRpmPackages
   class Sqlite < MiqRpmPackages
+    def initialize(fs, dbFile)
+      @fs           = fs
+      @db_file_path = dbFile
+
+      @rpmdb_tempfile = nil
+      @rpmdb_path     = nil
+
+      if fs.present?
+        rpmdb_file      = fs.fileOpen(@db_file_path, "r")
+        @rpmdb_tempfile = Tempfile.new("rpmdb", :binmode => true)
+
+        loop do
+          chunk = rpmdb_file.read(1024)
+          break if chunk.nil?
+
+          @rpmdb_tempfile.write(chunk)
+        end
+
+        @rpmdb_tempfile.close
+        rpmdb_file.close
+
+        @rpmdb_path = @rpmdb_tempfile.path
+      else
+        @rpmdb_path = @db_file_path
+      end
+    end
+
+    def each
+    end
+
+    def close
+      if @rpmdb_tempfile
+        @rpmdb_tempfile.close
+        @rpmdb_tempfile.unlink
+      end
+    end
   end
 end
